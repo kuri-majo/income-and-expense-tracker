44b37ef60ca4b08b4e1efc634d5eb4fb178c9917
Ira Kurthen <ira.kurthen@hotmail.com>
2026-03-14 13:27:34 +0100

    add yearly plots and logging

--- income_and_expense_tracker/income_and_expense_tracker.py
+++ income_and_expense_tracker/income_and_expense_tracker.py
@@ -3,6 +3,7 @@ import itertools
 import pandas as pd
 import plotly.graph_objects as go
 import xlwings as xw
+from loguru import logger
 
 
 def _get_data(wb):
@@ -15,13 +16,20 @@ def _get_data(wb):
 
 
 def _clean_data(df):
+    # cast integer columns to integers
+    int_columns = ["TransaktionsID", "Monat", "Jahr"]
+    df[int_columns] = df[int_columns].apply(lambda x: x.astype("Int32"))
+
     # remove transactions with no value (i.e., placeholders)
     df = df.dropna(subset="Wert")
 
     return df
 
 
-def _prepare_data(df):
+def _prepare_yearly_data(df, year):
+    # keep only transactions for the requested year
+    df = df[df["Jahr"] == year]
+
     # put into long format
     # first, keep all transactions that surely come from a source
     source_transactions = df[[col for col in df.columns if col != "Kategorie"]]
@@ -39,7 +47,7 @@ def _prepare_data(df):
     return agg_df
 
 
-def _create_sankey_plot(agg_df):
+def _create_sankey_plot(agg_df, year):
     # define labels, i.e., levels in the Sankey plot
     label_columns = ["Quelle", "Ziel"]
     labels = list(set(itertools.chain.from_iterable([set(agg_df[x]) for x in label_columns])))
@@ -49,7 +57,7 @@ def _create_sankey_plot(agg_df):
     agg_df["Ziel_idx"] = agg_df["Ziel"].apply(lambda x: labels.index(x))
 
     fig = go.Figure(
-        layout={"width": 1600, "height": 900, "font_size": 20},
+        layout={"width": 1600, "height": 900, "font_size": 20, "title": f"Money Flow {year}"},
         data=[
             go.Sankey(
                 node=dict(
@@ -71,25 +79,38 @@ def _create_sankey_plot(agg_df):
     return fig
 
 
+def _add_sheet_if_not_exists(wb, sheet_name: str):
+    try:
+        sheet = wb.sheets(sheet_name)
+    except Exception:
+        logger.warning(f"Requested sheet does not seem to exist")
+        logger.info(f"Adding requested sheet called '{sheet_name}'")
+        sheet = wb.sheets.add(sheet_name)
+    return sheet
+
+
 def main():
     wb = xw.Book.caller()
 
     df = _get_data(wb)
     df = _clean_data(df)
-    agg_df = _prepare_data(df)
 
-    fig = _create_sankey_plot(agg_df)
+    for year in df["Jahr"].unique():
+        agg_df = _prepare_yearly_data(df, year)
+        fig = _create_sankey_plot(agg_df, year)
 
-    # add image to "Plots" sheet
-    plots_sheet = wb.sheets["Plots"]
-    plots_sheet.pictures.add(
-        fig,
-        name="Money Flow",
-        update=True,
-        format="svg",
-    )
+        # add figure to the Plots sheet for the year
+        plots_sheet = _add_sheet_if_not_exists(wb, f"Plots {year}")
+        plots_sheet.pictures.add(
+            fig,
+            name=f"Money Flow {year}",
+            update=True,
+            format="svg",
+        )
 
-    print("Done")
+        logger.info(f"Added summary plot for year {year}")
+
+    logger.info("Done")
 
 
 if __name__ == "__main__":
